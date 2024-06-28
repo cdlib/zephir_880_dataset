1e51d70f37c18c113fb842df5b8d6b0894c522b9
Charlie Collett <charlie.collett@ucop.edu>
2024-06-27 19:30:56 -0700

    Add print status and time

--- create_880_record_dataset.py
+++ create_880_record_dataset.py
@@ -2,7 +2,9 @@ import pandas as pd
 import mysql.connector
 from dotenv import load_dotenv
 import os
+import time
 import pymarc
+from pymarc.exceptions import MissingLinkedFields
 
 # Load environment variables
 load_dotenv()
@@ -24,7 +26,8 @@ def get_title_from_marc(marc):
     # Get the linked 880 field associated with the 245 field
     try:
         linked_880s = record.get_linked_fields(field245)
-    except pymarc.exceptions.MissingLinkingField as e:
+    except MissingLinkedFields as e:
+        print(f"An error occurred: {e}")
         linked_880s = None
 
     # If there is a linked 880 field, return the title and the formatted 880 field
@@ -33,13 +36,26 @@ def get_title_from_marc(marc):
     
     return title, None
 
+# save the start time
+start_time = time.time()
+print("starting time: ", start_time)
+
 # load the csv file into pandas
-df = pd.read_csv('880_volumes.csv')
+print("loading data")
+load_time = time.time()
+df = pd.read_csv('880_volumes_dataset.csv')
+print("loading time: ", time.time()-load_time)
 
 # drop duplicates using cid and contribsys_id
+print("dropping duplicates")
+drop_time = time.time()
 df = df.drop_duplicates(subset=['cid', 'contribsys_id'], keep='first')
+print("drop time: ", time.time()-drop_time)
 
 # add expected selection order for zephir exports
+print("adding selection order")
+select_time = time.time()
+
 selection_order = 1
 current_cid = None
 for index, row in df.iterrows():
@@ -54,7 +70,7 @@ df['selection_order'] = df['selection_order'].astype(int)
 
 # drop columns for selection order calculation
 df.drop(columns=['var_usfeddoc', 'var_score', 'vufind_sort'], inplace=True)
-
+print("selection order time: ", time.time() - select_time)
 
 # use env variables for database connection
 DB_USERNAME = os.getenv("DB_USERNAME")
@@ -71,19 +87,27 @@ mydb = mysql.connector.connect(
   database=DB_DATABASE
 )
 
+
 # Template for your SQL query
 query = "SELECT id as htid, metadata_json FROM zephir_filedata WHERE id IN ({});"
+
 
 # Chunk requests to the database for efficiency
 id_list = df['htid'].to_list()
 chunk_size = 10000
 chunks = [id_list[x:x+chunk_size] for x in range(0, len(id_list), chunk_size)]
+num_chunks = len(chunks)
 
-for chunk in chunks:
+print("starting query")
+start_query_time = time.time()
+# Iterate over the chunks with index
+for i, chunk in enumerate(chunks):
     # Format the chunk into a comma-separated list of quoted IDs
     comma_id_list_chunk = ','.join(["'{}'".format(i) for i in chunk])
     formatted_query = query.format(comma_id_list_chunk)
 
+    print("Querying chunk {} of {}".format(i+1, num_chunks))
+    query_time = time.time()
     # Execute the query
     with mydb.cursor() as mycursor:
         mycursor.execute(formatted_query)
@@ -97,12 +121,16 @@ for chunk in chunks:
             # Add the title and 880 field to the dataframe
             df.loc[df['htid'] == htid, 'title'] = title
             df.loc[df['htid'] == htid, '880'] = field880
-
+    print("query finished: ", time.time() - query_time)
 # Close the database connection
 mydb.close()
-
-
+print("total query time for all records: ", time.time()-start_query_time)
 
 # write the dataframe to a tsv file
+print("writing file")
+write_time = time.time()
 df.to_csv('880_record_dataset.tsv', sep='\t', index=False)
+print("write time: ", time.time()-write_time)
 
+# print the time taken to run the script
+print("total time: ", time.time()-start_time)
--- create_880_volumes_dataset.py
+++ create_880_volumes_dataset.py
@@ -26,14 +26,15 @@ query = """
 SELECT cid, zr.namespace, zr.contribsys_id, zr.id as htid, zr.language, var_usfeddoc, var_score, concat(cid,'_',zr.autoid) as vufind_sort
 FROM zephir_records as zr inner join zephir_filedata as zf on zr.id = zf.id
 WHERE attr_ingest_date is not null
-AND zf.metadata LIKE '%tag="880"%<code="6">245-%'
+AND zf.metadata LIKE '%tag="880"%code="6">245-%'
 order by cid, var_usfeddoc DESC, var_score DESC, vufind_sort ASC
 """
-
+print("starting query")
 # write the results to a csv file
 with mydb.cursor() as mycursor:
   mycursor.execute(query)
-  with open('880_volumes.csv', mode='w', newline='') as file:
+  print("writing file")
+  with open('880_volumes_dataset.csv', mode='w', newline='') as file:
       writer = csv.writer(file)
       writer.writerow([i[0] for i in mycursor.description]) # write headers
       for row in mycursor:
